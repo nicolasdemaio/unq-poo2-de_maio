3ab0d73e4020f8c8419ccdf1a6b1e8043802d62c
Nicolas De Maio <nicolas.demaio19@gmail.com>
2020-09-12 16:46:03 -0300

    Prueba

---
+++ src/ar/edu/unq/po2/tp3/Consigna2.java
@@ -0,0 +1,33 @@
+package ar.edu.unq.po2.tp3;
+
+public class Consigna2 {
+
+	public static void main(String[] args) {
+		
+		String a = "abc";
+		String s = a;
+		String t;
+		
+		//System.out.println(s.length()); // 3
+		
+		//System.out.println(t.length()); // No inicializada
+		
+		//System.out.println(1 + a); // "1abc"
+		
+		//System.out.println(a.toUpperCase()); // "ABC"
+		
+		//System.out.println("Libertad".indexOf("r")); // 4
+		
+		//System.out.println("Universidad".lastIndexOf('i')); // 7
+		
+		//System.out.println("Quilmes".substring(2, 4)); // "il"
+		
+		//System.out.println((a.length() + a).startsWith("a")); // false
+		
+		//System.out.println(s == a); // true
+		
+		//System.out.println(a.substring(1, 3).equals("bc")); // true
+		
+	}
+	
+}
